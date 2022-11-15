0ee42a671b677df6fb395315c2b84ca0f374e497
Godwin Adigun <adigungodwin2@gmail.com>
2022-11-15 09:52:39 +0100

    echoing GET

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,20 @@
+import json
 from django.http import JsonResponse
 
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({'message': 'Hi, there. This your your Django Api Response!!!'})
+    print(request.GET)
+    print(request.POST)
+    
+    body = request.body
+    data = {}
+    try:
+        data = json.loads(body) # String of Json data to Python Dict
+    except:
+        pass
+    print(data)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    data['params'] = dict(request.GET)
+    # print(request.headers)
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -3,9 +3,9 @@ import requests
 # endpoint = "https://httpbin.org/status/200"
 # endpoint = "https://httpbin.org/anything"
 
-endpoint = "http://localhost:8000/api"
+endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, json={'query': 'Hello world'}) # HTTP Request
-print(get_response.text) # Prints raw text response
-print(get_response.status_code)
-print(get_response.json()['message'])
+get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello world"}) # HTTP Request
+# print(get_response.text) # Prints raw text response
+# print(get_response.status_code)
+print(get_response.json())
