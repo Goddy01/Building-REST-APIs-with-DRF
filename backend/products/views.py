6605b36ce8c6c0b3e61257560fc0f9eaafd80d61
Godwin Adigun <adigungodwin2@gmail.com>
2022-11-16 12:00:26 +0100

    added an API func for product detail requests

--- backend/products/views.py
+++ backend/products/views.py
@@ -3,6 +3,7 @@ from .models import Product
 from .serializers import ProductSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 class ProductCreateAPIView(generics.CreateAPIView):
     queryset = Product.objects.all()
@@ -41,7 +42,7 @@ class ProductListAPIView(generics.ListAPIView):
 
 @api_view(['GET', 'POST'])
 def product_alt_view(request, pk=None, *args, **kwargs):
-    if request.method == 'POST': # Could either be create or update
+    if request.method == 'POST': # Could either be a create or update request
         data = request.data
         serializer = ProductSerializer(data=data, many=False)
         if serializer.is_valid():
@@ -53,5 +54,9 @@ def product_alt_view(request, pk=None, *args, **kwargs):
             serializer.save(content=content)
         return Response(serializer.data)
 
-    elif request.method == 'GET': # Could either be detail or list
-        pass
+    elif request.method == 'GET': # Could either be a detail or list request
+        if pk is not None: # Checking if pk is present, if so, it means it is a product detail request
+            obj = get_object_or_404(Product, pk=pk)
+            serializer = ProductSerializer(obj, many=False).data
+            return Response(serializer)
+        
